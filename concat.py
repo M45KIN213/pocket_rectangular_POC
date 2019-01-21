c39df13cca1b7b766e225dad7c097fa11e0674cd
Emeric OSTERMEYER <emeric.ostermeyer@gmail.com>
2019-01-21 15:22:05 +0100

    Update concat.py

--- concat.py
+++ concat.py
@@ -1,18 +1,13 @@
 from random import randint
-import tensorflow as tf                 # Tensorflow, developpé par Google, une excellente librairie ML
-from tensorflow.keras import layers     # KERAS est une surcouche plus haut niveau, qui me permet de manipuler des
-                                        # objets TF sans trop m'embêter - Esprit "Le ML n'est qu'un outil"
+import tensorflow as tf
+from tensorflow.keras import layers
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import concatenate
-import numpy as np                      # NUMPY, manipulation de données, matrices, vecteurs, des maths quoi
-import pandas as pd                     # PANDAS, manipulation de données, inclus un petit utilitaire TRES pratique pour ouvrir des CSV
+import pandas as pd
 import tensorflow.keras.backend as K
 from tensorflow.python.ops import math_ops
-from tensorflow.python.framework import tensor_util
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import math
 import os
 
 a = input('Nb of cycles ?')
@@ -23,20 +18,6 @@ except:
 if a > 1:
     plt.ioff()
 
-nice_string = """Machine this pocket using :
-
-Operation : {} {}
-Strategy  : {}
-
-With allowances :
-
-Radial    : {}
-Axial     : {}
-
-Requirements on the tool :
-
-Mx Radius : {}
-Mx R_beak : {}"""
 
 def display_metrics(history_op, history_strat, history_float):
     fig = plt.figure(figsize=[30, 20])
@@ -100,49 +81,6 @@ def display_metrics(history_op, history_strat, history_float):
 
     return fig
 
-def display_results(model, pocket_desc = [140.0, 44.0, 16.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]):
-    # Evaluation sur un cas test et affichage :
-    print("Description de la poche :\n ", pocket_desc)
-    my_X = tf.constant([pocket_desc])
-
-    Y = model.predict(my_X, steps = 1)
-    Y = K.eval(K.sum(Y, axis = 0))
-
-    # Pour affichage des différentes valeurs de sorties :
-    # for i in range(len(headerOutput)-1):
-    #     print("{} : {}".format(headerOutput[i+1], Y[i]))
-
-    for i in range(len(Y)):
-        print("{} : {}".format(headerOutput[i+1], Y[i]))
-    # Juste pour cosmétique, avec "nice_string" défini en tête de script.
-    op_type_index = K.eval(math_ops.argmax(Y[0:3]))
-    quality = 0 if Y[4]>0.5 else 1
-    strategy_index = K.eval(math_ops.argmax(Y[5:11]))
-    allowance_rad = Y[12]
-    allowance_bot = Y[13]
-    tool_description = Y[14:]
-
-    etat_intermediaire = [0, 0, 0, 0, 0]
-
-    etat_intermediaire[0] = Y[0] - 2*allowance_rad
-    etat_intermediaire[1] = Y[1] - 2*allowance_rad
-    etat_intermediaire[2] = Y[2] - 2*allowance_bot
-    #etat_intermediaire[3] = math.floor(Y[14])+1
-    #etat_intermediaire[4] = math.floor(Y[15])+1
-
-    #etat_intermediaire.append(K.eval(my_X)[0:-5])
-
-    #print("Etat intermédiaire : \n", etat_intermediaire)
-
-    op_type = op_list[op_type_index]
-    strat = strat_list[strategy_index]
-
-    print('\n\n\t\t####################################################################\n\n')
-
-    print(nice_string.format(op_type, ["finish", ""][quality], strat, allowance_rad, allowance_bot, tool_description[0], tool_description[1]))
-
-    return(etat_intermediaire)
-
 def normalize_pocket_desc(dataset):
     dataset["l"] = dataset["l"]/dataset["L"]
     dataset["h"] = dataset["h"]/dataset["L"]
@@ -152,10 +90,12 @@ def normalize_pocket_desc(dataset):
 
     dataset["Rcoin"] = dataset["Rcoin"]/dataset["L"]
     dataset["Rbot"] = dataset["Rbot"]/dataset["L"]
-    # "Rcoin_rea","Rbot_rea"
+
+    # Tool cond are debatable
     dataset["Rcoin_rea"] = dataset["Rcoin_rea"]/dataset["L"]
     dataset["Rbot_rea"] = dataset["Rbot_rea"]/dataset["L"]
-    # "allow_bot","Allow_side"
+
+    # Allowances are debatable
     dataset["allow_bot"] = dataset["allow_bot"]/dataset["L"]
     dataset["Allow_side"] = dataset["Allow_side"]/dataset["L"]
 
@@ -163,64 +103,11 @@ def normalize_pocket_desc(dataset):
     dataset["l_act"] = dataset["l_act"] / dataset["L"]
     dataset["h_act"] = dataset["h_act"] / dataset["L"]
 
-
-
-    dataset["L"] = dataset["L"]/150
+    dataset["L"] = dataset["L"]/150 # Max size of the pockets
 
     return dataset
 
-def my_metric_custom(y_true, y_pred):
-    """
-    L'objectif de cette métrique est de caractériser la prédiction - y_pred - par rapport à la caleur "réelle" - y_true
-    On a une prédiction composée de différents éléments ; deux sets de booleens mutuellement exclusifs, un booleen simple
-    et des réels.
-
-    On se propose de séparer la métrique en 5 parties :
-    >> Le type d'opération :
-          "bot_side_mill","plane_mill","side_mill","drilling"
-    >> La qualité :
-          "finish"
-    >> La stratégie :
-          "Unidir_mill","Bidir_mill","Contour_para", "Bidir_contour","Contour_spiral","Center_mill","Explicit"
-    >> Les surépaisseurs :
-          "allow_bot","Allow_side"
-    >> La caractérisation outil :
-          "Rcoin_rea","Rbot_rea"
-    """
-    # "bot_side_mill","plane_mill","side_mill","drilling"
-    op_type = 1 - K.cast(K.equal(
-        K.argmax(y_true[:, 0:3], axis = -1),
-        K.argmax(y_pred[:, 0:3], axis = -1))
-        , K.floatx())
-
-    # "finish"
-    quality = 1 - K.cast(K.equal(
-        K.argmax(y_true[:, 4:5], axis = -1),
-        K.argmax(y_pred[:, 4:5], axis = -1))
-        , K.floatx())
-
-    # "Unidir_mill","Bidir_mill","Contour_para", "Bidir_contour","Contour_spiral","Center_mill","Explicit"
-    strategy = 1 - K.cast(K.equal(
-        K.argmax(y_true[:, 5:11], axis = -1),
-        K.argmax(y_pred[:, 5:11], axis = -1))
-        , K.floatx())
-
-    # "allow_bot","Allow_side"
-    # Check : mean_squared_logarithmic_error
-    allowances = K.mean(math_ops.square(y_pred[:, 12:13] - y_true[:, 12:13]), axis=-1)
-    # Mean Sq Err : Will give important loss, where the boolean will just give 0 or 1
-    # >> Check something that'll be more "normalised"
-
-    # "Rcoin_rea","Rbot_rea"
-    tool_cond = K.mean(math_ops.square(y_pred[:, 14:15] - y_true[:, 14:15]), axis=-1)
-
-    # https://www.tensorflow.org/api_docs/python/tf/concat
-    value = K.mean(tf.stack([op_type, quality, strategy, allowances, tool_cond]), 1)
-    print(value)
-    return value
-
-
-# Pour constater l'évolution suivant l'intégralité des catégories :
+# Metrics are defined for each 'atomic' choice :
 def my_metric_op_type(y_true, y_pred):
     return tf.keras.losses.categorical_crossentropy(y_true, y_pred)
 
@@ -236,13 +123,9 @@ def my_metric_allowances(y_true, y_pred):
 def my_metric_tool_cond(y_true, y_pred):
     return K.mean(math_ops.square(y_pred - y_true))
 
-def my_metrics_no_treatment(y_true, y_pred):
-    return K.cast(y_true - y_pred, K.floatx())
-
-
-# Ouverture du fichier csv contenant le dataset, avec la bibliothèque pandas, que toutes les bonnes bibliothèques de ML comprennent ;)
+# Open the csv file containing the dataset
 for i in range(a):
-    dataset_csv = pd.read_csv("venv/mats/app.csv")
+    dataset_csv = pd.read_csv("app.csv")
 
     header = [
         "NAME","L","l","h","Rcoin","Rbot","Open","Al","Ti","Acier","isPlan","L_act","l_act","h_act","Rcoin_act","Rbot_act",
@@ -270,18 +153,10 @@ for i in range(a):
     # Actual conversion
     dataset_csv[header_numeric] = dataset_csv[header_numeric].astype('float32')
 
-
-
     dataset_csv = normalize_pocket_desc(dataset_csv)
 
     headerInput = [
         "NAME","L","l","h","Rcoin","Rbot","Open","Al","Ti","Acier","isPlan","L_act","l_act","h_act","Rcoin_act","Rbot_act"
-    ]
-
-    headerOutput = [
-        "NAME", "bot_side_mill","plane_mill","side_mill","drilling","finish","Unidir_mill",
-        "Bidir_mill","Contour_para","Bidir_contour","Contour_spiral","Center_mill",
-        "Explicit","allow_bot","Allow_side","Rcoin_rea","Rbot_rea"
     ]
 
     headerOutputOP = [
@@ -297,26 +172,26 @@ for i in range(a):
         "NAME", "allow_bot","Allow_side","Rcoin_rea","Rbot_rea"
     ]
 
-    # Pour affichage des opérations à réaliser :
+    # For further treatment, ex when using the networks to predict on a new case
     op_list = ["bot_side_mill","plane_mill","side_mill","drilling"]
     strat_list = ["Unidir_mill", "Bidir_mill","Contour_para", "Bidir_contour", "Contour_spiral", "Center_mill", "Explicit"]
     allowances = ["allow_bot","Allow_side"]
     tool_cond = ["Rcoin_rea","Rbot_rea"]
 
-    # On extrait 10% des cas pour la validation :
+    # 20% of the dataset is used for validation purposes
     index = []
     for i in range(int(len(dataset_csv) / 5)-1):
         index.append(randint(1, 5) + i*5)
 
     val_data = dataset_csv.loc[index]
-
+    # Remove the cases used for validation
     dataset = dataset_csv.drop(index, axis = 0)
-
+    # A Dataset item is used for the training
     dataset_OP = tf.data.Dataset.from_tensor_slices((dataset[headerInput].drop("NAME", axis = 1),
                                                   (
                                                        dataset[op_list],
                                                        dataset['finish']
-                                                   ))
+                                                   )) # Shuffled and packed into batches of 10 OP
                                                  ).shuffle(7, reshuffle_each_iteration = True).batch(10).repeat()
 
     val_data_OP = tf.data.Dataset.from_tensor_slices((val_data[headerInput].drop("NAME", axis = 1),
@@ -326,7 +201,7 @@ for i in range(a):
                                                    ))
                                                   ).shuffle(5, reshuffle_each_iteration = True).batch(5).repeat()
 
-    # On extrait 10% des cas pour la validation :
+    # Same operation is performed for the strategy field...
     index = []
     for i in range(int(len(dataset) / 5)):
         index.append(randint(1, 5) + i*5)
@@ -345,7 +220,7 @@ for i in range(a):
                                                          val_data[strat_list])
                                                         ).shuffle(5, reshuffle_each_iteration = True).batch(5).repeat()
 
-    # On extrait 10% des cas pour la validation :
+    # ... and for the tool_cond and allowances
     index = []
     for i in range(int(len(dataset) / 5)):
         index.append(randint(1, 5) + i*5)
@@ -368,41 +243,33 @@ for i in range(a):
                                                    ))
                                                   ).shuffle(5, reshuffle_each_iteration = True).batch(5).repeat()
 
-
+    # Definition of the networks :
+    # One input layer
     visible = Input(shape = (15,))
-
-    #normalisation_layer_op = layers.BatchNormalization()(visible)
-
-    #hidden_1_op = layers.Dense(8, activation = 'tanh')(normalisation_layer_op)
-
+    # Two hidden layers with 8 neurons each
     hidden_1_op = layers.Dense(8, activation = 'tanh')(visible)
 
     hidden_2_op = layers.Dense(8, activation = 'tanh')(hidden_1_op)
-
+    # And a two-fold output layer
     op_type = layers.Dense(4, activation = 'softmax')(hidden_2_op) # softmax : Gives 1 to the most probable output
     quality = layers.Dense(1, activation = 'tanh')(hidden_2_op) # tanh : Strong delimitation between outputs -1 and 1
 
-
-    # output_layer = concatenate([op_type, quality, op_strat, allow_and_tool_cond])
-
+    # My model has an input and an output ; the links between the layers are created when defining the layers
     model_OP = Model(inputs = visible, outputs = [op_type, quality])
 
-
-    # my_opti = tf.keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-
+    # Parametered optimizer
     my_opti_op = tf.keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
 
+    # The model is instantiated by defining the optimiser, loss functions and metrics
     model_OP.compile(
-    #              optimizer=tf.train.RMSPropOptimizer(0.001),
                   optimizer = my_opti_op,
                   loss = [my_metric_op_type,
                           my_metric_quality],
-    #              loss=tf.keras.losses.categorical_crossentropy,
                   metrics = {
                       'op_type' : my_metric_op_type,
                       'quality' : my_metric_quality
                   } )
-
+    # And then trained on the dataset and validation set defined earlier.
     history_OP = model_OP.fit(dataset_OP, epochs = 50, steps_per_epoch = 20, validation_data = val_data_OP, validation_steps=5, shuffle=True)
 
     ########################################################################################
@@ -413,17 +280,7 @@ for i in range(a):
     ########        ########        ########        ########        ########        ########
     ########################################################################################
 
-    # visible_input = Input(shape = (15,))
-    # visible_op = Input(shape = (4,))
-    # visible_quality = Input(shape=(1,))
-    #
-    # visible = concatenate([visible_input, visible_op, visible_quality])
-
     visible = Input(shape = (20,))
-
-    # normalisation_layer_strat = layers.BatchNormalization()(visible)
-    #
-    # hidden_1_strat = layers.Dense(8, activation = 'tanh')(normalisation_layer_strat)
 
     hidden_1_strat = layers.Dense(8, activation = 'tanh')(visible)
 
@@ -431,24 +288,16 @@ for i in range(a):
 
     op_strat = layers.Dense(7, activation = 'softmax')(hidden_2_strat)
 
-
-    # output_layer = concatenate([op_type, quality, op_strat, allow_and_tool_cond])
-
     model_strat = Model(inputs = visible, outputs = op_strat)
-
-
-    # my_opti = tf.keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
     my_opti_strat = tf.keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
 
     model_strat.compile(
-    #              optimizer=tf.train.RMSPropOptimizer(0.001),
                   optimizer = my_opti_strat,
                   loss = my_metric_strategy,
-    #              loss=tf.keras.losses.categorical_crossentropy,
                   metrics = {
                       'strat' : my_metric_strategy
-                  } )
+                  })
 
     history_strat = model_strat.fit(dataset_strat, epochs = 50, steps_per_epoch = 20,
                                     validation_data = val_data_strat, validation_steps=5, shuffle=True)
@@ -463,66 +312,43 @@ for i in range(a):
 
     visible = Input(shape = (15,))
 
-    # normalisation_layer_float = layers.BatchNormalization()(visible)
-    #
-    # hidden_1_float = layers.Dense(8, activation = 'tanh')(normalisation_layer_float)
-
     hidden_1_float = layers.Dense(8, activation = 'tanh')(visible)
 
     hidden_2_float = layers.Dense(8, activation = 'tanh')(hidden_1_float)
 
-    allow = layers.Dense(2, activation = 'relu')(hidden_2_float) # Allows for predictions in the [0;inf[ range
+    allow = layers.Dense(2, activation = 'relu')(hidden_2_float)
     tool_cond = layers.Dense(2, activation = 'relu')(hidden_2_float)
 
-
-    # output_layer = concatenate([op_type, quality, op_strat, allow_and_tool_cond])
-
     model_float = Model(inputs = visible, outputs = [allow, tool_cond])
-
-
-    # my_opti = tf.keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
     my_opti_op = tf.keras.optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
 
     model_float.compile(
-    #              optimizer=tf.train.RMSPropOptimizer(0.001),
                   optimizer = my_opti_op,
                   loss = [my_metric_allowances,
                           my_metric_tool_cond],
-    #              loss=tf.keras.losses.categorical_crossentropy,
                   metrics = {
                       'allowances' : my_metric_allowances,
                       'tool_cond' : my_metric_tool_cond
-                  } )
+                  })
 
     history_float = model_float.fit(dataset_float, epochs = 50, steps_per_epoch = 20,
                                  validation_data = val_data_float, validation_steps=5, shuffle=True)
 
-    # a = input('Affichage du graphe :\n0 - Non\n1 - Oui')
-    # if a == "1":
+
+    # Display the training history of the three networks
     myFig = display_metrics(history_OP, history_strat, history_float)
-    #
-    # # display_results(model)
-    #
-    # # Todo : Boucler s/ nouvel état "as-is" de la poche, après OP n°1
-    # print("\nThat's all folks !")
-    #
+
+    # Save model and training history in graphical form
     directory = r'Saved_models\new_dir'
     i = 0
     while os.path.isdir(directory + str(i)):
         i = i + 1
-    #
+
     directory = directory + str(i)
-    #
+
     os.mkdir(directory)
-    #
-    # model_json = model.to_json()
-    # with open(directory +  r'\model.json', 'w') as json_file:
-    #     json_file.write(model_json)
-    #
-    #
-    # model.save_weights(directory + r'\model.h5')
-    #
+
     myFig.savefig(directory + r'\model.png')
     model_float_json = model_float.to_json()
     with open(directory +  r'\model_float.json', 'w') as json_file:
